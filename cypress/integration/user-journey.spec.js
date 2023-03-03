cebf10c7bca92b98f5ba21333c3e8f96bc9ffc17
Hermann EHO <hermannleboss@gmail.com>
2023-03-03 09:23:19 +0000

    feat: user journey and sommes testion on others pages

--- cypress/integration/user-journey.spec.js
+++ cypress/integration/user-journey.spec.js
@@ -1,5 +1,5 @@
 describe('User Journey', () => {
-  it.only('Should display properly and navigate to month page', () => {
+  it('Should display properly and navigate to month page', () => {
     cy.visit('/')
     cy.get('h2').contains('Welcome to Celebrity Fanalyzer!')
     cy.getByData('month-link').click()
